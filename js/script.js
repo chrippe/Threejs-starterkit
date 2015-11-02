b9e9b9d3cb8cea6a0c6b4d5bcf5878f7b78cd0c0
chrippe <cek@cek.se>
2015-11-02 09:46:29 +0100

    Fixed loader

--- js/script.js
+++ js/script.js
@@ -37,37 +37,38 @@ function init() {
   container.appendChild(renderer.domElement);
 
   //STATS
-    stats = new Stats();
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.bottom = '0px';
-    stats.domElement.style.zIndex = 100;
-    container.appendChild(stats.domElement);
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.bottom = '0px';
+  stats.domElement.style.zIndex = 100;
+  container.appendChild(stats.domElement);
 
   // LIGHT
-    var light = new THREE.PointLight(0xffffff);
-    light.position.set(100,250,100);
-    scene.add(light);
+  var light = new THREE.PointLight(0xffffff);
+  light.position.set(100,250,100);
+  scene.add(light);
 
-    // FLOOR
+  // FLOOR
   var floorTextureLoader = new THREE.TextureLoader();
-    floorTextureLoader.load('images/checkerboard.jpg', 
-                            function (texture){
-                            texture.wrapS = texture.wrapT =THREE.RepeatWrapping;
-                            texture.repeat.set(10,10);
-                            var floorMaterial = new THREE.MeshBasicMaterial({map:texture, side:THREE.DoubleSide});
-                            var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
-	                        var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-	                        floor.position.y = -0.5;
-	                        floor.rotation.x = Math.PI / 2;
-	                        scene.add(floor);
-    });
+  floorTextureLoader.load('images/checkerboard.jpg',
+  function (texture){
+    texture.wrapS = texture.wrapT =THREE.RepeatWrapping;
+    texture.repeat.set(10,10);
+    var floorMaterial = new THREE.MeshBasicMaterial({map:texture, side:THREE.DoubleSide});
+    var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.position.y = -0.5;
+    floor.rotation.x = Math.PI / 2;
+    scene.add(floor);
+  });
+
 	// SKYBOX
 	var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
 	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
 	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
 	scene.add(skyBox);
 
-  ////////////
+////////////
 // CUSTOM //
 ////////////
 
